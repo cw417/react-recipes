a3d512db2e5155af23a1fe0233fd19ff6f0f5e60
cw417 <chriswhitlock417@gmail.com>
2022-08-15 18:59:56 -0400

    Initial setup for local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  
+  const [recipes, setRecipes] = useState([])
+  const [prevRecipes, setPrevRecipes] = useState([])
+  const recipeNameRef = useRef()
+  const searchRef = useRef()
+
+  const LOCAL_STORAGE_KEY = 'recipeApp.recipes'
+
+  useEffect(() => {
+    const storedRecipes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedRecipes) setRecipes(storedRecipes)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+  
   return (
     <div className="App">
-      Hello World
+      Recipe App
     </div>
   );
 }
